0c89a3c07e568618c0196a54c686043ee281a6e8
Peter <pete225848@gmail.com>
2021-11-01 15:42:37 -0700

    created color randomizer with interal

--- src/container/RandomColor.jsx
+++ src/container/RandomColor.jsx
@@ -6,6 +6,30 @@ class RandomColor extends Component {
     currentColor: '#800000'
   }
 
+  colorRandomizer = () => {
+    const arrOfColors = [
+      '#191970',
+      '#7FFF00',
+      '#DDA0DD',
+      '#FFD700',
+      '#FFA07A',
+      '#FFB6C1',
+      '#B22222',
+    ]
+
+    return arrOfColors[Math.floor(Math.random() * arrOfColors.length)];
+  }
+
+  colorInterval = () => {
+    setInterval(() => {
+      this.setState({ currentColor: this.colorRandomizer() })
+    }, 1000)
+  }
+
+  componentDidMount = () => {
+    this.colorInterval()
+  }
+
   render() {
     return <ColorSquare currentColor={this.state.currentColor} />
   }
