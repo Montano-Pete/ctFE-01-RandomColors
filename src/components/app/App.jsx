6496656e9ce95269b2e2298b35a25fd8b782aba6
Peter <pete225848@gmail.com>
2021-11-01 15:01:37 -0700

    rendering colored square on page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ColorSquare from '../square/ColorSquare';
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <ColorSquare />
 }
---
+++ src/components/square/ColorSquare.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ColorSquare = () => {
+  return <div style={{ width: 500, height: 500, backgroundColor: 'yellow' }}></div>
+};
+
+export default ColorSquare;
